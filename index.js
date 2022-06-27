754953e9e492af0990c00736be0af3144c040232
lukas <liesislukas@gmail.com>
2022-06-27 19:19:42 +0300

    up

--- index.js
+++ index.js
@@ -12,130 +12,188 @@ export default function useUrl() {
     navigate = null;
     searchParams = null;
 
+    protocol = "";
+    domain = "";
+    domainWithPort = "";
+    port = "";
+    path = "/";
+    fragment = "";
+
     constructor(location, navigate, searchParams) {
       this.location = location;
       this.navigate = navigate;
       this.searchParams = searchParams;
+
+      this.protocol = window?.location?.protocol;
+      this.domain = window?.location?.hostname;
+      this.domainWithPort = window?.location?.host;
+      this.port = window?.location?.port;
+      this.path = this?.location?.pathname || "/";
+      this.fragment = window?.location?.hash;
+    }
+
+    getProtocol() {
+      return this.protocol;
+    }
+
+    getDomain() {
+      return this.domain;
+    }
+
+    getDomainWithPort() {
+      return this.domainWithPort;
+    }
+
+    getPort() {
+      return this.port;
+    }
+
+    getPath() {
+      return this.path;
+    }
+
+    getQuery() {
+      return this.searchParams.toString();
+    }
+
+    getFragment() {
+      return this.fragment;
+    }
+
+    getUri() {
+      let _uriParts = [];
+      if (this.getProtocol()) {
+        _uriParts.push(`${this.getProtocol()}//`);
+      }
+      if (this.getDomainWithPort()) {
+        _uriParts.push(this.getDomainWithPort());
+      }
+      _uriParts.push(this.getUriWithoutDomain());
+      return _uriParts.join("");
+    }
+
+    getUriWithoutDomain() {
+      let _uriParts = [];
+      _uriParts.push(this.getPath() || "/");
+      if (this.getQuery() && !this.getFragment()) {
+        _uriParts.push(`?${this.getQuery()}`);
+      }
+      if (!this.getQuery() && this.getFragment()) {
+        _uriParts.push(`${this.getFragment()}`);
+      }
+      if (this.getQuery() && this.getFragment()) {
+        _uriParts.push(`?${this.getQuery()}${this.getFragment()}`);
+      }
+      return _uriParts.join("");
+    }
+
+    get(variable) {
+      let result;
+      if (variable) {
+        let _result = this.searchParams.getAll(variable);
+        if (_result?.length === 1) {
+          result = _result[0];
+        } else if (_result?.length > 1) {
+          result = _result;
+        }
+      }
+      return result;
+    }
+
+    removeQuery() {
+      this.searchParams = new URLSearchParams({});
+      return this;
+    }
+
+    removeFragment() {
+      this.fragment = "";
+      return this;
+    }
+
+    removePath() {
+      this.path = "/";
+      return this;
+    }
+
+    setPath(value, doKeepQuery) {
+      if (doKeepQuery !== true) {
+        this.removeQuery();
+        this.removeFragment();
+      }
+      this.path = value || "/";
+      return this;
+    }
+
+    setFragment(value) {
+      value = value || "";
+      if (value.slice(0, 1) !== "#") {
+        value = `#${value}`;
+      }
+      this.fragment = value || "";
+      return this;
+    }
+
+    set(variable, value) {
+      if (variable) {
+        if (value === null) {
+          value = undefined;
+        }
+
+        if (typeof value === "undefined") {
+          this.searchParams.delete(variable);
+        } else if (typeof value === "string") {
+          this.searchParams.set(variable, value);
+        } else if (typeof value === "object" && Array.isArray(value)) {
+          this.searchParams.delete(variable);
+          if (value.length > 0) {
+            value.forEach((_value) => {
+              this.searchParams.append(variable, _value);
+            });
+          }
+        } else {
+          this.searchParams.set(variable, JSON.stringify(value));
+        }
+      }
+      return this;
+    }
+
+    add(variable, value) {
+      if (variable && typeof variable === "string") {
+        let _current = this.searchParams.getAll(variable);
+        if (_current.includes(value) === false) {
+          this.searchParams.append(variable, value);
+        }
+      }
+      return this;
+    }
+
+    subtract(variable, value) {
+      if (variable) {
+        let _current = this.searchParams.getAll(variable);
+        this.searchParams.delete(variable);
+        _current = _current.filter((x) => x !== value);
+        if (_current.length > 0) {
+          _current.forEach((x) => this.searchParams.append(variable, x));
+        }
+      }
+      return this;
+    }
+
+    delete(variable) {
+      if (variable) {
+        this.searchParams.delete(variable);
+      }
+      return this;
+    }
+
+    push() {
+      navigate(this.getUriWithoutDomain(), { replace: false });
+    }
+
+    replace() {
+      navigate(this.getUriWithoutDomain(), { replace: true });
     }
   }
 
-  let url = new Url(location, navigate, searchParams);
-
-  // let url = {
-  //   searchParams,
-  //   location,
-  //   navigate,
-  //
-  //   protocol: window?.location?.protocol,
-  //   domain: window?.location?.hostname,
-  //   domainWithPort: window?.location?.host,
-  //   port: window?.location?.port,
-  //   path: this?.location?.pathname || "/",
-  //   fragment: window?.location?.hash,
-  //
-  //   getUri: () => {
-  //     let _uriParts = [];
-  //     if (this.getProtocol()) {
-  //       _uriParts.push(`${this.getProtocol()}//`);
-  //     }
-  //     if (this.getDomainWithPort()) {
-  //       _uriParts.push(this.getDomainWithPort());
-  //     }
-  //     _uriParts.push(this.getUriWithoutDomain());
-  //     return _uriParts.join("");
-  //   },
-  //   getUriWithoutDomain: () => {
-  //     let _uriParts = [];
-  //     _uriParts.push(this.getPath() || "/");
-  //     if (this.getQuery() && !this.getFragment()) {
-  //       _uriParts.push(`${this.getQuery()}`);
-  //     }
-  //     if (!this.getQuery() && this.getFragment()) {
-  //       _uriParts.push(`#${this.getFragment()}`);
-  //     }
-  //     if (this.getQuery() && this.getFragment()) {
-  //       _uriParts.push(`?${this.getQuery()}#${this.getFragment()}`);
-  //     }
-  //     return _uriParts.join("");
-  //   },
-  //   getProtocol: () => this.protocol,
-  //   getDomain: () => this.domain,
-  //   getDomainWithPort: () => this.domainWithPort,
-  //   getPort: () => this.port,
-  //   getPath: () => this.path,
-  //   getQuery: () => this.searchParams.toString(),
-  //   getFragment: () => this.fragment,
-  //   get: (variable) => {
-  //     let result;
-  //     if (variable) {
-  //       let _result = this.searchParams.getAll(variable);
-  //       if (_result?.length === 1) {
-  //         result = _result[0];
-  //       } else if (_result?.length > 1) {
-  //         result = _result;
-  //       }
-  //     }
-  //     return result;
-  //   },
-  //   removeQuery: () => {
-  //     this.searchParams = new URLSearchParams({});
-  //     return this;
-  //   },
-  //   removeFragment: () => {
-  //     this.fragment = "";
-  //     return this;
-  //   },
-  //   removePath: () => {
-  //     this.path = "/";
-  //     return this;
-  //   },
-  //   setPath: (value) => {
-  //     this.path = value || "/";
-  //     return this;
-  //   },
-  //   set: (variable, value) => {
-  //     if (variable && typeof variable === "string") {
-  //       if (typeof value === "undefined") {
-  //         this.searchParams.delete(variable);
-  //       }
-  //
-  //       if (typeof value === "string") {
-  //         this.searchParams.set(value);
-  //       }
-  //
-  //       if (typeof value === "object" && Array.isArray(value)) {
-  //         this.searchParams.delete(variable);
-  //         if (value.length > 0) {
-  //           value.forEach((_value) => {
-  //             this.searchParams.append(variable, _value);
-  //           });
-  //         }
-  //       }
-  //     }
-  //     return this;
-  //   },
-  //   add: (variable, value) => {
-  //     if (variable && typeof variable === "string") {
-  //       let _current = this.searchParams.getAll(variable);
-  //       if (_current.includes(value) === false) {
-  //         this.searchParams.append(variable, value);
-  //       }
-  //     }
-  //     return this;
-  //   },
-  //   remove: (variable) => {
-  //     if (variable) {
-  //       this.searchParams.delete(variable);
-  //     }
-  //     return this;
-  //   },
-  //   push: () => {
-  //     navigate(this.getUriWithoutDomain(), { replace: false });
-  //   },
-  //   replace: () => {
-  //     navigate(this.getUriWithoutDomain(), { replace: true });
-  //   },
-  // };
-
-  return url;
+  return new Url(location, navigate, searchParams);
 }
